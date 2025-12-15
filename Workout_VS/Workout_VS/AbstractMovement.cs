c469a888e20b2d6f6b6e04c60d329aad5279dcb3
Алексей <manay.ru@mail.ru>
2025-12-16 09:31:33 +1000

    Create new class AbstractMovement

---
+++ Workout_VS/Workout_VS/AbstractMovement.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workout_VS
+{
+    internal abstract class AbstractMovement : IMovement
+    {
+        public abstract int Speed { get; set; }
+
+        public abstract void Move();
+    }
+}
